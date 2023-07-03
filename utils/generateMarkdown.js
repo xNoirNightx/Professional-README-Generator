b990f30520febb19e806c092630a8a594b6da645
Coral <cnight18@hotmail.com>
2023-07-03 10:12:33 -0300

    adding more const and #

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,28 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+const licenseBadge = renderLicenseBadge;
+const licenseSection = renderLicenseSection; 
+const licenseLink renderLicenseLink;
+
   return `# ${data.title}
+
+
+## Description
+## Table of Contents
+## Installation
+## Usage
+## Tests
+## Contributing
+## Questions
+For additional questions, please contact:
+- GitHub:
+- Email:
 
 `;
 }
