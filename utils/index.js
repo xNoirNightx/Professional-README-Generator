2333a956024353f2d43f99917ccc947f397f3299
Coral <cnight18@hotmail.com>
2023-07-03 10:03:45 -0300

    adding const

--- utils/index.js
+++ utils/index.js
@@ -1,13 +1,57 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'project title:'
+      },
+      {
+        type: 'input',
+        name: 'title',
+        message: ':'
+      },
+      {
+        type: 'input',
+        name: 'title',
+        message: ':'
+      },
+      {
+        type: 'input',
+        name: 'title',
+        message: ':'
+      },
+      {
+        type: 'input',
+        name: 'title',
+        message: ':'
+      },
+      {
+        type: 'input',
+        name: 'title',
+        message: ':'
+      },
+      {
+        type: 'input',
+        name: 'title',
+        message: ':'
+      },
+    //   add more prompts here for future edits
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeToFile
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((answers)
+}
 
 // Function call to initialize app
 init();
