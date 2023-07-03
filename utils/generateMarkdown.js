dff38ba2b8fe359c7fef3a7827058af47b07cf53
Coral <cnight18@hotmail.com>
2023-07-03 10:50:43 -0300

    fixed const errors , added $data

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -6,21 +6,32 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-const licenseBadge = renderLicenseBadge;
-const licenseSection = renderLicenseSection; 
-const licenseLink renderLicenseLink;
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseLink = renderLicenseLink(data.license);
+  const licenseSection = renderLicenseSection(data.license);
 
   return `# ${data.title}
 
 
 ## Description
+${data.description}
+
 ## Table of Contents
+
 ## Installation
+${data.installation}
+
 ## Usage
+${data.usage}
+
 ## Tests
+${data.tests}
+
 ## Contributing
+${data.contributing}
+
 ## Questions
-For additional questions, please contact:
+Any Questions ? , Please contact Me:
 - GitHub:
 - Email:
 
